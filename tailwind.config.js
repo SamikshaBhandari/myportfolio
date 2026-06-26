f9e20b84b7298804d6bf7c0cefdab8f72893c80f
Samikshya Bhandari <samikshyabhandari522@gmail.com>
2026-06-26 16:54:01 +0545

    update portfolio

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    // १. यसले रिएक्टका कुन-कुन फाइलमा स्टाइल लागू गर्ने भनेर खोज्छ
     content: [
         "./index.html",
         "./src/**/*.{js,ts,jsx,tsx}",
